bfe7aaefe4cbecca6688a02b9a28d2780ae42a7d
Kristen Borges <kb0rg@users.noreply.github.com>
2020-02-20 18:20:03 -0800

    Migrate module from thesis infrastructure
    This is the inital copy, without changes

--- main.tf
+++ main.tf
@@ -1,1 +1,37 @@
+resource "kubernetes_namespace" "tiller" {
+  metadata {
+    annotations {
+      description = "Namespace dedicated to tiller install."
+    }
 
+    name = "${var.tiller_namespace_name}"
+  }
+
+  count = "${var.tiller_namespace_name == "kube-system" ? 0 : 1}"
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "${var.tiller_namespace_name}"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller-cluster-role-binding" {
+  metadata {
+    name = "tiller-cluster-admin-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.tiller.metadata.0.name}"
+    namespace = "${var.tiller_namespace_name}"
+    api_group = ""
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
-
+variable "tiller_namespace_name" {
+  description = "The name of the namespace you want tiller to live in."
+  type        = "string"
+  default     = ""
+}
